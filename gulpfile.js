c9c3b60342e3ecb5fda6e1a60e937b93dfa53c28
DenisMaier <maierd1812@outlook.de>
2022-05-27 20:28:49 +0200

    promo

--- gulpfile.js
+++ gulpfile.js
@@ -12,21 +12,22 @@ gulp.task('server', function() {
             baseDir: "src"
         }
     });
+
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src('src/sass/**/*.+(scss|sass)')
             .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-            .pipe(rename({suffix: '.min', prefix: '',}))
+            .pipe(rename({suffix: '.min', prefix: ''}))
             .pipe(autoprefixer())
             .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest("src/css"))
+            .pipe(gulp.dest('src/css'))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));

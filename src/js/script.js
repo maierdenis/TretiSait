0e6a0b967661ced55c2dd1c5383a1178ad9b3f1e
DenisMaier <maierd1812@outlook.de>
2022-06-03 18:43:28 +0200

    validate

--- src/js/script.js
+++ src/js/script.js
@@ -52,5 +52,35 @@ $(document).ready(function(){
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
 
-    
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введён почтовый адрес"
+                }
+            }
+        });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
   });
